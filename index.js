38957bff66c07bbe04f2d2cc5f773bce7339bec5
Catalina <cvajiac@nd.edu>
2018-03-24 12:45:37 -0400

    initial threejs stuff

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var path = require('path');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 8080;
+var three = require('three');
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
