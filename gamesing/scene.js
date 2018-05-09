2803de7df4c3012360f625f1dcdcf844b85ea8dc
Quarke <tflanagan94@me.com>
2018-05-09 09:46:37 -0400

    Changes?

--- gamesing/scene.js
+++ gamesing/scene.js
@@ -1,12 +1,14 @@
 import path from 'path';
 import * as PHYSICS from './physics/physics-module.module.js'
 import io from 'socket.io-client'
-const socket = io('http://localhost:3000')
+const socket = io('http://174.138.58.39:3000')
 
 const info = {}
 let gamestate = []
 let players = {}
 let terrain = {}
+
+let sphere_color = 0xff0000
 
 function throttled(delay, fn) {
   let lastCall = 0;
@@ -81,9 +83,10 @@ app.get('renderer').shadowMap.autoUpdate = false
 // Start the app
 app.start();
 
-const update_handler = throttled(25, () => {
-  socket.emit('player_move', {
+const update_handler = throttled(20, () => {
+  socket.emit('client_update', {
       id: info.id, 
+      color: info.color,
       pos: sphere.position, 
       terrain: {}
     }
@@ -102,6 +105,7 @@ socket.on('greeting', (msg) => {
 })
 
 socket.on('loadup', (data) => {
+  Object.assign(terrain, data)
   let box
   for(let key of Object.keys(data)) {
     //key is uuidv4, data[key] to pos and color
@@ -140,54 +144,56 @@ socket.on('loadup', (data) => {
   }
 })
 
-socket.on('id_res', (data) => {
-  info.id = data
+socket.on('id_res', (client_obj) => {
+  // receive a client object
+  Object.assign(info, client_obj)
+  sphere.material = new THREE.MeshBasicMaterial({ color: client_obj.color })
 })
 
 socket.on('state', (data) => {
-  gamestate = data
+  Object.assign(gamestate, data)
 })
 
 // Manage other players here
 const gamestate_loop = new WHS.Loop(() => {
-    for (let item of Object.values(gamestate)) {
-      if(item.id === info.id){
-        continue
-      }
-      if (item.id in players) {
-        players[item.id].position = item.pos
-      }
-      else {
-        console.log(item)
-        //player is new, create a WHS object for them
-        const s = new WHS.Sphere({
-          geometry: {
-            radius: 1,
-            widthSegments: 12,
-            heightSegments: 12
-          },
-
-          modules: [
-            new PHYSICS.SphereModule({
-              mass: 1,
-              restitution: 1
-            })
-          ],
-
-          material: new THREE.MeshBasicMaterial({
-            color: 0xFF0000
-          }),
-
-          position: new THREE.Vector3(0, 0, 0)
-        })
-
-        s.addTo(app)
-
-        players[item.id] = s
-      }
+  console.log(gamestate)
+  for (let item of Object.values(gamestate)) {
+    if(item.id === info.id || !item.id){
+      continue
     }
+    if (item.id in players) {
+      players[item.id].position = item.pos
+    }
+    else {
+      console.log(item)
+      //player is new, create a WHS object for them
+      const s = new WHS.Sphere({
+        geometry: {
+          radius: 1,
+          widthSegments: 12,
+          heightSegments: 12
+        },
+
+        modules: [
+          new PHYSICS.SphereModule({
+            mass: 1,
+            restitution: 1
+          })
+        ],
+
+        material: new THREE.MeshBasicMaterial({
+          color: item.color
+        }),
+
+        position: new THREE.Vector3(0, 0, 0)
+      })
+
+      s.addTo(app)
+
+      players[item.id] = s
+    }
+  }
 })
 
 gamestate_loop.start(app)
-
 
--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const io = require('socket.io')(server)
 
 console.log("Server started at localhost:3000")
 
+let colors = [0x009933, 0x009999, 0x33cc99, 0x663399, 0x993300, 0x9966cc, 0xcc0000, 0xcc6600, 0xccccff, 0xff99ff]
+let used_colors = []
+
+let players = {}
 let terrain = {}
 
 for (var i = 200; i > 0; i--) {
@@ -21,8 +25,6 @@ for (var i = 200; i > 0; i--) {
 
 server.listen(3000);
 
-g_data = {}
-
 io.on('connect', (socket) => {
   console.log("Client joined")
 
@@ -30,25 +32,36 @@ io.on('connect', (socket) => {
   io.emit('greeting', 'Hello clients... Are you ready to play a game')
   socket.emit('loadup', terrain)
 
-  socket.on('player_move', (data) => {
-    g_data[data.id] = data
-    socket.broadcast.emit('state', g_data);
+  socket.on('client_update', (data) => {
+    console.log(data)
+    players[data.id] = data
+    Object.assign(terrain, data.terrain);
+    socket.broadcast.emit('state', players);
   })
 
   socket.on('id_req', () => {
-    const id = uuidv4()
-    socket.id = id
-    socket.emit('id_res', id)
+    let unused_color = colors.filter(x => !used_colors.includes(x))
+    const c = unused_color[Math.floor(Math.random()  * unused_color.length)]
+
+    let client_obj = {
+        id: uuidv4(),
+        color: c
+    }
+    // Store info in socket, and refresh the player array
+    socket.id = client_obj.id
+    players[client_obj.id] = {}
+
+    socket.emit('id_res', client_obj)
   })
 
   socket.on('disconnect', () => {
-    delete g_data[socket.id]
+    delete players[socket.id]
   })
 }); 
 
 require('draftlog').into(console)
 
-const draft = console.draft(g_data)
+const draft = console.draft(players)
 setInterval( () => {
-    draft(g_data || null)
+    draft(players || null)
 }, 100)
